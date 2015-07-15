b0f44a8e5a45b07bfe628d3e8951be170746c8f7
Mike Green <mike.is.green@gmail.com>
2015-07-15 17:28:28 -0400

    Added markdown parsing

--- source/js/comment.jsx
+++ source/js/comment.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import marked from 'marked';
 
 class Comment extends React.Component {
   render() {
+    let rawMarkup = marked(this.props.children.toString(), { sanitize: true });
+
     return (
       <li>
         <h2>{this.props.author}</h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
       </li>
     );
   }
