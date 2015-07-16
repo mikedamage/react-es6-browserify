9cafe3105b5911ae1f5e0d9924f14ff6ada23873
Mike Green <mike.is.green@gmail.com>
2015-07-16 10:19:37 -0400

    Using non-native map function

--- source/js/comment-list.jsx
+++ source/js/comment-list.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import map     from 'lodash.map';
+import React   from 'react';
 import Comment from './comment';
 
 class CommentList extends React.Component {
   render() {
-    let commentNodes = this.props.data.map(comment => {
+    let commentNodes = map(this.props.data, comment => {
       return (
         <Comment key={comment.key} author={comment.author}>
           {comment.text}
